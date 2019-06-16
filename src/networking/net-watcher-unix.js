56fe0aced075686d82d7f5eee98033a92e3e01b8
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-06-16 15:49:03 +0800

    Add socket connection

---
+++ src/networking/net-watcher-unix.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const fs = require("fs");
+const net = require("net");
+
+const fileName = process.argv[2];
+
+if (!fileName) throw Error("No file name specified.");
+
+net.createServer(connection => {
+    console.log("Subscriber connected.");
+    connection.write(`Watching ${fileName} for changes.\n`);
+
+    const watcher = fs.watch(fileName, () => {
+        connection.write(`File changed: ${new Date()}.\n`);
+    });
+
+    connection.on("close", () => {
+        console.log("Subscriber disconnected.");
+        watcher.close();
+    });
+}).listen(
+    "/tmp/watcher.sock",
+    () => console.log(`Listening for subscribers...`)
+);
